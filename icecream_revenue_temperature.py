2dd79168e7eec76d7f34210c62b3061714bcb035
Anubhav Kumar Gupta <117196999+akg30@users.noreply.github.com>
2023-08-26 20:19:20 +0530

    Add files via upload

---
+++ icecream_revenue_temperature.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 26 11:40:04 2023
+
+@author: Hp
+"""
+
+import streamlit as st
+import numpy as np
+import pickle as pk
+
+loaded_model=pk.load(open('C:\Ice cream revenue/trained_model.sav','rb'))
+
+def revenue(input_temperature):
+    input_temperature_array=np.asarray(input_temperature)
+    input_temperature_array_new=np.reshape(input_temperature_array, (1,-1))
+    revenue_predict=loaded_model.predict(input_temperature_array_new)
+    import math 
+    return 'The Revenue Based On Temperature {1} Celcius is {0} Rupees'.format(math.floor(revenue_predict),input_temperature)
+
+def main():
+    st.title('Ice Cream Sales Revenue Prediction By Temperature Using Machine Learning')
+    temp=st.number_input('Enter Temperature')
+    revenue_show=' '
+    if st.button('Click Here To Get Revenue'):
+        revenue_show=revenue(temp)
+    st.success(revenue_show)
+    st.subheader('Exploratory Data Analysis Done And Machine Learning Deployed By "Anubhav Kumar Gupta"')
+
+if __name__=="__main__":
+    main()
+              
