c95286caca33c12dca76498da14e417fee5babfd
Anubhav Kumar Gupta <117196999+akg30@users.noreply.github.com>
2023-08-27 17:21:16 +0530

    Update icecream_revenue_temperature.py

--- icecream_revenue_temperature.py
+++ icecream_revenue_temperature.py
@@ -22,7 +22,7 @@ def main():
     st.title('Ice Cream Sales Revenue Prediction Using Machine Learning')
     temp=st.number_input('Enter Temperature')
     revenue_show=' '
-    if st.button('Click Here To Get Revenue'):
+    if st.button('Click Here To Get Expected Revenue'):
         revenue_show=revenue(temp)
     st.success(revenue_show)
     st.subheader('Exploratory Data Analysis Done And Machine Learning Model Deployed By "Anubhav Kumar Gupta"')
